f6c535e3cca2f4d819f8a82605a8f0534d74d64d
jun199004 <1059701824@qq.com>
2018-09-20 21:17:42 +0800

    first

--- loginsvr/src/main/java/com/app/loginsvr/controller/TestController.java
+++ loginsvr/src/main/java/com/app/loginsvr/controller/TestController.java
@@ -17,6 +17,7 @@ public class TestController {
         Date d= CommMethod.getCurDate();
         SimpleDateFormat df=new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
         return "loginsvr当前时间："+df.format(d);
+        //ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJzOf/Gmqj2ZHbxyRLnun7qgGbMHVP4pW/dkXjPt+eSmdFmANkFx2FvCP7quZSo4cGnQcPaXeaGdkDXFUz9kVrJqVgcDe6mwjBo6J3O4yC9vqCbInPMeZZ5UxpVAAK2r2Jjw2L4+VetGvFkjcxqtVS1U+ykFArBo+XgmAV/fhmU5NYoPboGbi2tyZUGhYFAmIWXXDd6QMl3VFLzqVk/qpIcQTP53UfWST5qL3vJZl7MkKPQsZzWBi1eQLUQZAU/ezAdhHml1Fed1CJvz17V8JiLrJG/kg+nTbSumkwaIgv5M4dcDww5rAlxIqQqYyUMFtQPuqHBbdXN9RF9ZMXjQwd
     }
 }
 
